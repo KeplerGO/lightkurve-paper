d8373f835af72b76272928dbaba0304b8df07be5
mirca <jvmirca@gmail.com>
2018-02-09 16:50:42 -0800

    move paper to aas template

--- scripts/fold_planet.py
+++ scripts/fold_planet.py
@@ -12,9 +12,9 @@ rc('font', **font)
 lc_file = KeplerLightCurveFile("https://archive.stsci.edu/missions/kepler/lightcurves/"
                                "0119/011904151/kplr011904151-2009350155506_llc.fits")
 
-klc = lc_file.get_lightcurve("PDCSAP_FLUX").fold(period=0.837495)
-plt.plot(klc.time, klc.flux/1e5, 'ko', markersize=3)
-plt.plot(klc.time, klc.flux/1e5, 'o', color='#ff3366', markersize=2)
-plt.ylabel(r"Flux $(10^5e^{-}s^{-1})$")
+klc = lc_file.PDCSAP_FLUX.flatten().fold(period=0.837495)
+plt.plot(klc.time, klc.flux, 'ko', markersize=3)
+plt.plot(klc.time, klc.flux, 'o', color='#ff3366', markersize=2)
+plt.ylabel(r"Flux $e^{-}s^{-1}$")
 plt.xlabel(r"Phase")
 plt.savefig('fold-lc.eps', bbox_inches='tight', pad_inches=.1)
